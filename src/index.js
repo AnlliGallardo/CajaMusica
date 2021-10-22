496c233007a0e40a4f23f689ed2790fa3cc0f104
AnlliGallardo <87675145+AnlliGallardo@users.noreply.github.com>
2021-10-22 16:26:51 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import AppCaja from './components/AppCaja'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+   <AppCaja />  
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
